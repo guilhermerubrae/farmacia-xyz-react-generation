3796a8eabe1d77a6a8a0825d73cb7f76f31ec996
guilhermerubrae <guilhermerubrae@gmail.com>
2024-02-28 18:58:04 -0300

    up rotas commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-
-import './App.css';
-import Home from './paginas/home/Home';
-
-import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
 import Navbar from './components/navBar/NavBar';
+import Home from './paginas/home/Home';
 import ListaCategoria from './components/categoria/listaCategoria/ListaCategoria';
 import FormularioCategoria from './components/categoria/formularioCategoria/FormularioCategoria';
 import DeletarCategoria from './components/categoria/deletarCategoria/DeletarCategoria';
+import Footer from './components/footer/Footer';
 
 
 function App() {
+
   return (
     <>
-    <BrowserRouter>
-        <Navbar />
-          <div className='min-h-[80vh]'>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
-              <Route path="/categoria" element={<ListaCategoria />} />
+              <Route path="/categorias" element={<ListaCategoria />} />
               <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
               <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
-              <Route path='/deletarCategoria/:id' element={<DeletarCategoria />} />
+              <Route path="/deletarCategoria/:id" element={<DeletarCategoria/>} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
     </>
-);
+  )
 }
-export default App;
+
+export default App
