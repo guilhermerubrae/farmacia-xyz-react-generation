a12fa3535f9f6c284da32152cf397cc381e1bfde
guilhermerubrae <guilhermerubrae@gmail.com>
2024-02-28 18:54:37 -0300

    up crud commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Home from './paginas/home/Home';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navBar/NavBar';
+import ListaCategoria from './components/categoria/listaCategoria/ListaCategoria';
+import FormularioCategoria from './components/categoria/formularioCategoria/FormularioCategoria';
+import DeletarCategoria from './components/categoria/deletarCategoria/DeletarCategoria';
 
 
 function App() {
@@ -16,6 +19,10 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/categoria" element={<ListaCategoria />} />
+              <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
+              <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
+              <Route path='/deletarCategoria/:id' element={<DeletarCategoria />} />
             </Routes>
           </div>
           <Footer />
